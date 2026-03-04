5b0ba130b3f09aa1f8409bb360bad1474232b123
weareaisle-demo-app <Iv23linzHARThdPwQevh+weareaisle-demo-app@users.noreply.github.com>
2026-03-04 18:45:56 +0000

    Apply fix for issue: Out-of-Bounds Write in corrupt_signature() Function

--- apps/lib/apps.c
+++ apps/lib/apps.c
@@ -3283,7 +3283,8 @@ void corrupt_signature(const ASN1_STRING *signature)
 {
     unsigned char *s = signature->data;
 
-    s[signature->length - 1] ^= 0x1;
+    if (signature->length > 0)
+        s[signature->length - 1] ^= 0x1;
 }
 
 int check_cert_time_string(const char *time, const char *desc)
