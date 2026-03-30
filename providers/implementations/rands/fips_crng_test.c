af8a36e63728997119dd86de9d28bbf3296e0955
weareaisle-demo-app <Iv23linzHARThdPwQevh+weareaisle-demo-app@users.noreply.github.com>
2026-03-30 17:07:06 +0000

    Apply fix for issue: Memory Leak in FIPS CRNG Health Test

--- providers/implementations/rands/fips_crng_test.c
+++ providers/implementations/rands/fips_crng_test.c
@@ -298,7 +298,10 @@ static size_t crng_test_get_seed(void *vcrngt, unsigned char **pout,
         if (n == 0)
             return 0;
         r = crng_test(crngt, *pout, n);
-        return r > 0 ? n : 0;
+        if (r > 0)
+            return n;
+        ossl_prov_cleanup_entropy(crngt->provctx, *pout, n);
+        return 0;
     }
 
     /* Grab seed from our parent */
